6268dc703a7956708f0c655537b47914e01f4516
joshuaybang <joshuaybang@gmail.com>
2021-04-21 05:31:18 +0000

    lab4

---
+++ labs/lab4/student/tests/q3.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q3',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': '>>> W.shape\n(1288, 200)', 'hidden': False, 'locked': False},
+                                   {'code': '>>> H.shape\n(200, 1850)', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.round(sum(H), 2)\narray([261.43, 275.91, 279.15, ..., 229.19, 229.8 , 232.47], dtype=float32)', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
